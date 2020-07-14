ca7a8cc0a0dda5ada531096f1d88a20a2e4ae0f0
Ricardo Castro <rickhy86@gmail.com>
2020-07-14 11:39:21 -0700

    primer commit desde gitkraken

---
+++ pull.php
@@ -0,0 +1,22 @@
+<?php
+
+    $persona = [
+        "nombre" => "Pedro",
+        "apellido" => "Sánchez",
+        "edad" => 50
+    ];
+
+    foreach ($persona as $valor){
+        echo "$valor <br>";
+    }
+
+    $auto = [
+        "Marca" => "Ford",
+        "Modelo" => "Focus",
+        "Año" => 2017
+    ];
+
+    foreach ($auto as $posicion => $valor){
+        echo "$posicion: $valor <br>";
+    }
+?>
